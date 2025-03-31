d8e4893ab0421e698c47729cb310e9020c3fd1c7
matzmoto <lemon_bj@naver.com>
2025-03-31 12:47:11 +0900

    modify it

--- main.cpp
+++ main.cpp
@@ -12,26 +12,12 @@ TEST(deviceTest, memoryReadSucces)
 {
 	FlashMemoryDeviceMock mockFMD;
 	DeviceDriver dD(&mockFMD);
-	int a = 5;
-	int matchValue = 2;
-
+	
 	int fakeAddress = 0x100;
-
-	int matchCount = 0;
-
-
 
 	EXPECT_CALL(mockFMD, read(fakeAddress)).WillRepeatedly(Return((unsigned char)2));
 
-	for (int i = 0; i < 5; i++)
-	{
-		if (matchValue == dD.read(fakeAddress))
-		{
-			matchCount++;
-		}
-	}
-
-	EXPECT_THAT(matchCount, Eq(5));
+	EXPECT_THAT(dD.read(fakeAddress), Eq(0));
 
 }
 
@@ -40,30 +26,57 @@ TEST(deviceTest, memoryReadFail)
 {
 	FlashMemoryDeviceMock mockFMD;
 	DeviceDriver dD(&mockFMD);
-	int a = 5;
-	int matchValue = 2;
+
+	int fakeAddress = 0x100;
+
+
+	EXPECT_CALL(mockFMD, read(fakeAddress)).WillOnce(Return((unsigned char)2))
+	.WillOnce(Return((unsigned char)2))
+	.WillOnce(Return((unsigned char)2))
+	.WillOnce(Return((unsigned char)2))
+	.WillOnce(Return((unsigned char)1));
+
+	EXPECT_THAT(dD.read(fakeAddress), Ne(0));
+
+}
+
+
+TEST(deviceTest, memoryWriteSucces)
+{
+	FlashMemoryDeviceMock mockFMD;
+	DeviceDriver dD(&mockFMD);
+	int data = 5;
+	int matchValue = 0xFF;
+
+	int fakeAddress = 0x100;
+
+	EXPECT_CALL(mockFMD, read(fakeAddress)).WillOnce(Return((unsigned char)matchValue));
+	EXPECT_CALL(mockFMD, write(fakeAddress, data)).Times(1);
+	
+	dD.write(fakeAddress, data);
+
+}
+
+TEST(deviceTest, memoryWriteFail)
+{
+	FlashMemoryDeviceMock mockFMD;
+	DeviceDriver dD(&mockFMD);
+	int data = 5;
+	int matchValue = 0xFF;
 
 	int fakeAddress = 0x100;
 
 	int matchCount = 0;
 
 
-
-	EXPECT_CALL(mockFMD, read(fakeAddress)).WillRepeatedly(Return((unsigned char)2));
-
-	for (int i = 0; i < 5; i++)
-	{
-		if (i == 4)
-		{
-			EXPECT_CALL(mockFMD, read(fakeAddress)).WillOnce(Return((unsigned char)3));
+	EXPECT_CALL(mockFMD, read(fakeAddress)).WillOnce(Return((unsigned char)0));
+	
+	EXPECT_THROW
+	(
+		{ 
+				dD.write(fakeAddress, data);
 		}
-		if (matchValue == dD.read(fakeAddress))
-		{
-			matchCount++;
-		}
-	}
-
-	EXPECT_THAT(matchCount, Ne(5));
+	, WriteFailException);	
 
 }
 
